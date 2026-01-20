231d0e84b0135b6c0537dad7b72707f4f7894b17
Manmit Pal <manmitpal348@gmail.com>
2026-01-20 23:40:33 +0530

    Implemented Dark Theme and theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+import {mode} from "@chakra-ui/theme-tools"
 import './index.css'
 import App from './App.jsx'
 
+const styles = {
+  global:(props) => ({
+    body:{
+      color:mode('gray.800', 'whiteAlpha.900')(props),
+      bg:mode('gray.100', '#101010')(props)
+    }
+  })
+};
+
+const config = {
+   initialColorMode: "dark",
+   useSystemColorMode: true
+};
+
+const colors = {
+  gray:{
+    light: "#616161",
+    dark: "#1e1e1e"
+  }
+};
+
+const theme = extendTheme({config, styles, colors});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <ChakraProvider>
+      <ChakraProvider theme={theme} >
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>,
