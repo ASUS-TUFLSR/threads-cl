beb8989e0ed8f56accc2e5098782fbf1f4c868f2
Manmit Pal <manmitpal348@gmail.com>
2026-01-25 11:44:43 +0530

    HEader created & dummy user's png added. still working on the userHeader and UserPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Button, Container } from '@chakra-ui/react'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import UserPage from './pages/UserPage'
+import PostPage from './pages/PostPage'
 
 const App = () => {
   return (
     <Container maxW="620px" >
+      <Header/>
       <Routes>
         <Route path="/:username" element={<UserPage/>} />
         <Route path="/:username/post/:pid" element={<PostPage/>} />
