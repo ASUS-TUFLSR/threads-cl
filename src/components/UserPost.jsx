64cd5b2984a855f7edaabc67c6ce6ef14b9aaffe
Manmit Pal <manmitpal348@gmail.com>
2026-02-06 11:37:41 +0530

    Working on UserPost

---
+++ src/components/UserPost.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+const UserPost = () => {
+  return (
+    <Link to={"/markz/post/1"} >UserPost</Link>
+  )
+}
+
+export default UserPost
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import UserHeader from '../components/UserHeader'
+import UserPost from '../components/UserPost';
 
 const UserPage = () => {
   return <>
     <UserHeader/>
+    <UserPost/>
   </>
 }
 
