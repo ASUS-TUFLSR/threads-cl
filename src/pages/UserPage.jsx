49f32cf0bfa8906967d04a96ef96f28293377f5d
Manmit Pal <manmitpal348@gmail.com>
2026-02-13 22:09:44 +0530

    img issue resolved

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,9 +5,9 @@ import UserPost from '../components/UserPost';
 const UserPage = () => {
   return <>
     <UserHeader/>
-    <UserPost likes={1200} replies={481} postImg="/post1.png" postTitle="Let's talk about threads." />
-    <UserPost likes={190} replies={81} postImg="/post2.png" postTitle="Let's talk about threads." />
-    <UserPost likes={100} replies={971} postImg="/post3.png" postTitle="Let's talk about threads." />
+    <UserPost likes={1200} replies={481} postImg="./post1.png" postTitle="Let's talk about threads." />
+    <UserPost likes={190} replies={81} postImg="./post2.png" postTitle="Let's talk about threads." />
+    <UserPost likes={100} replies={971} postImg="./post3.png" postTitle="Let's talk about threads." />
   </>
 }
 
