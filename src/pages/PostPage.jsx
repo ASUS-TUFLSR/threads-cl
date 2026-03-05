b1b1e32d80fd1ff9c5923ffce3371be9b9c5f18d
Manmit Pal <manmitpal348@gmail.com>
2026-03-05 20:34:07 +0530

    postpage

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,8 +1,25 @@
+import { Avatar, Image } from '@chakra-ui/react'
 import React from 'react'
+import { BsThreeDots } from 'react-icons/bs'
 
 const PostPage = () => {
   return (
-    <div>PostPage</div>
+    <>
+        <Flex>
+          <Flex w={"full"} alignItems={"center"} gap={3} >
+            <Avatar src="zuck-avatar.png" size={"md"} name='Mark Zuckerberg' />
+            <Flex>
+              <Text fontSize={"sm"} fontWeight={"bold"} >markzuckerberg</Text>
+              <Image src="/verified.png" w="4" h="4" ml={4} />
+            </Flex>
+          </Flex>
+          <Flex gap={4} alignItems="center" >
+            <Text fontSize={"sm"} color={"gray.light"} >1d</Text>
+            <BsThreeDots/>
+          </Flex>
+        </Flex>
+        
+    </>
   )
 }
 
