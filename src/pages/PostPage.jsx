14929f6ab33d7912a11080c40a500380fe276095
Manmit Pal <manmitpal348@gmail.com>
2026-03-17 12:35:14 +0530

    Improving PostPage and Comments will add real time data later.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,8 +1,13 @@
-import { Avatar, Image } from '@chakra-ui/react'
-import React from 'react'
+import { Avatar, Box, Button, Divider, Flex, Image, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import { BsThreeDots } from 'react-icons/bs'
+import Actions from '../components/Actions';
+import Comment from '../components/Comment';
 
 const PostPage = () => {
+
+  const [liked, setLiked] = useState(false);
+
   return (
     <>
         <Flex>
@@ -18,8 +23,45 @@ const PostPage = () => {
             <BsThreeDots/>
           </Flex>
         </Flex>
-        
+
+        <Text my={3} >Let's talk about threads</Text>
+
+        <Box borderRadius={6} overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"} >
+          <Image src={"/post1.png"} w={"full"} />
+        </Box>
+
+        <Flex gap={3} my={3} >
+            <Actions liked={liked} setLiked={setLiked} />
+        </Flex>
+
+        <Flex gap={2} alignItems={"center"} >
+          <Text color={"gray.light"} fontSize={"sm"} >238 replies</Text>
+          <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"} ></Box>
+          <Text color={"gray.light"} fontSize={"sm"} >
+            {200 + (liked ? 1 : 0)} likes
+             </Text>
+
+        </Flex>
+        <Divider my={4} />
+
+      <Flex justifyContent={"space-between"} >
+        <Flex gap={2} alignItems={"center"} >
+          <Text fontSize={"2xl"} >🤝🏻</Text>
+          <Text color={"gray.light"} >Get the app to like, reply and post.</Text>
+        </Flex>
+        <Button>
+          Get
+        </Button>
+      </Flex>
+
+        <Divider my={4} />
+        <Comment/>
+        <Comment/>
+        <Comment/>
+        <Comment/>
+
     </>
+
   )
 }
 
