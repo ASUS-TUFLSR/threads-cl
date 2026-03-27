732b07479b630c9ab9ed2bf50aaffd7855f721f1
Manmit Pal <manmitpal348@gmail.com>
2026-03-27 19:12:37 +0530

    userRoutes Created

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -41,6 +41,6 @@ const userSchema = mongoose.Schema({
     timeStamps: true,
 });
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
