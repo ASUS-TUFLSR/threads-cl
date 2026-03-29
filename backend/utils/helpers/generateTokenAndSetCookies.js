b40739d5e563b29c6f55b5be54a9bb5657bdde19
Manmit Pal <manmitpal348@gmail.com>
2026-03-29 15:29:44 +0530

    generateTokenAndSetCookies created

---
+++ backend/utils/helpers/generateTokenAndSetCookies.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+
+const generateTokenAndSetCookies = (userId, res) => {
+      
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {
+         expiresIn: "15d", 
+    });
+
+    res.cookie("jwt", token, {
+        httpOnly: true, // This cookie cannot be accessed by browser i.e More Secure
+        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 Days
+        sameSite: "strict", // CSRF attacks
+    });
+
+    return token;
+
+}
+
+export default generateTokenAndSetCookies;
