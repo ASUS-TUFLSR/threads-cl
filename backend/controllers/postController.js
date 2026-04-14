f5d5b0277288b55d28fcda1698aa85e6354df688
Manmit Pal <manmitpal348@gmail.com>
2026-04-14 11:40:31 +0530

    Getting Post as well as deleting post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -48,3 +48,23 @@ export const getPost = async(req, res) => {
             console.log("Unable to fetch posts", error.message)
         }   
 }
+
+export const deletePost = async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({message: "Post not found"});
+        }
+
+        if(post.postedBy.toString() !== req.user._id.toString()){
+            return res.status(401).json({message: "Unauthorized"})    
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({message: "Post deleted successfully!"})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Unable to delete post", error.message);
+    }
+}
