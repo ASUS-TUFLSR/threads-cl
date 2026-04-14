38b8126acdabc1bc0e5d6263fa62308e22a28f45
Manmit Pal <manmitpal348@gmail.com>
2026-04-14 12:08:32 +0530

    Like/Unlike Post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -68,3 +68,30 @@ export const deletePost = async(req, res) => {
         console.log("Unable to delete post", error.message);
     }
 }
+
+export const likeUnlikePost = async(req, res) => {
+    try {
+        const {id: postId} = req.params;
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId);
+
+        if(!post){
+            res.status(404).json({message: "Not found"});
+        }
+
+        const userLikePost = post.likes.includes(userId);
+        
+        if(userLikePost){
+            await Post.updateOne({_id: postId}, {$pull: {likes: userId}});
+            res.status(200).json({message: "Post unliked successfully"});
+        }else{
+            post.likes.push(userId);
+            await post.save();
+            res.status(200).json({message: "Post liked successfully"})
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in likeUnlike", error.message);
+    }
+}
