fa634d8824b12beb1b1b4a5205dbc084a622631b
Manmit Pal <manmitpal348@gmail.com>
2026-04-15 12:26:54 +0530

    reply to post added

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -95,3 +95,32 @@ export const likeUnlikePost = async(req, res) => {
         console.log("Error in likeUnlike", error.message);
     }
 }
+
+export const replyToPost = async(req, res) => {
+    try {
+        const {text} = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+        const userProfilePic = req.user.userProfilePic;
+        const username = req.user.username;
+
+        if(!text){
+            res.status(400).json({message: "Text field is required"});
+        }
+
+        const post = await Post.findById(postId);
+        if(!post){
+            res.status(404).json({message: "Post not found!"});
+        }
+
+        const reply = {userId, text, userProfilePic, username};
+
+        post.replies.push(reply);
+        await post.save();
+
+        res.status(200).json({message: "Reply added successfully", post})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in reply post", error.message);
+    }
+}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createPost, getPost, deletePost, likeUnlikePost } from "../controllers/postController.js";
+import { createPost, getPost, deletePost, likeUnlikePost, replyToPost } from "../controllers/postController.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get("/:id", getPost);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/reply/:id", protectRoute, replyToPost);
 
 export default router
