56aa9a3ede90507ac6422ba6f038205e23272ba3
Manmit Pal <manmitpal348@gmail.com>
2026-04-17 11:49:54 +0530

    Changed To Port 3000 and HomePage created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import UserPage from './pages/UserPage'
 import PostPage from './pages/PostPage'
+import HomePage from "./pages/HomePage"
 
 const App = () => {
   return (
     <Container maxW="620px" >
       <Header/>
       <Routes>
+        <Route path="/" element={<HomePage/>} />
         <Route path="/:username" element={<UserPage/>} />
         <Route path="/:username/post/:pid" element={<PostPage/>} />
       </Routes>
