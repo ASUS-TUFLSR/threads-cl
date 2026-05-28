373545c7147b667d1706d58f296756c49e1ff360
Manmit Pal <manmitpal348@gmail.com>
2026-05-28 14:44:00 +0530

    Converstion comp created

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,7 +1,8 @@
 import { Button } from '@chakra-ui/button'
 import { SearchIcon } from '@chakra-ui/icons'
-import { Box, Flex, Input, Text } from '@chakra-ui/react'
+import { Box, Flex, Input, Skeleton, SkeletonCircle, Text, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
+import Conversation from '../components/Conversation'
 
 const ChatPage = () => {
   return (
@@ -18,17 +19,46 @@ const ChatPage = () => {
         }}
         mx={"auto"}
        >
-          <Flex flex={30} >
-            <Text></Text>
+          <Flex flex={30} 
+                gap={2}
+                flexDirection={"column"}
+                maxW={{
+                  sm:"250px",
+                  md:"full"
+                }}
+                mx={"auto"}
+          >
+            <Text fontWeight={700} color={useColorModeValue("gray.600", "gray.400")} >
+              Your Conversations
+            </Text>
             <form>
-               <Flex alignItems={"center"} >
+               <Flex alignItems={"center"} gap={2} >
                   <Input placeholder='Search for a user' />
                   <Button size={"sm"} >
                     <SearchIcon/>
                   </Button>
-
+ 
                </Flex>
             </form>
+
+            {true && (
+               [0,1,2,3,4].map((_, i) => (
+                <Flex key={i} gap={4} alignItems={"center"} p={"1"} borderRadius={"md"} >
+                   <Box>
+                     <SkeletonCircle size={"10"}/>
+                   </Box>
+
+                   <Flex w={"full"} flexDirection={"column"} gap={3} >
+                      <Skeleton h={"10px"} w={"80px"} />
+                      <Skeleton h={"8px"} w={"90%"} />
+                   </Flex>
+                </Flex>
+               ))
+            )}
+            <Conversation/>
+            <Conversation/>
+            <Conversation/>
+
           </Flex>
           <Flex flex={70} >M Container</Flex>
        </Flex>
