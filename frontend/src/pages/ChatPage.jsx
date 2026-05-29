0bf8f14ff2fcbe6385f6b7bde1cab96d4af9c91f
Manmit Pal <manmitpal348@gmail.com>
2026-05-29 19:56:47 +0530

    Messaging Container crerated

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -3,20 +3,16 @@ import { SearchIcon } from '@chakra-ui/icons'
 import { Box, Flex, Input, Skeleton, SkeletonCircle, Text, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import Conversation from '../components/Conversation'
+import {GiConversation} from "react-icons/gi"
+import MessageContainer from '../components/MessageContainer'
 
 const ChatPage = () => {
   return (
     <Box position={"absolute"} p={4}
          left={"50%"} w={{base:"100%", md:"80%", lg:"750px"}} transform={"translateX(-50%)"}
     >
-       <Flex gap={4} flexDirection={{
-        base:"column",
-        md:"row"
-       }}
-        maxW={{
-          sm:"400px",
-          md:"full"
-        }}
+      <Flex gap={4} flexDirection={{ base:"column", md:"row"}}
+        maxW={{sm:"400px",md:"full"}}
         mx={"auto"}
        >
           <Flex flex={30} 
@@ -60,10 +56,14 @@ const ChatPage = () => {
             <Conversation/>
 
           </Flex>
-          <Flex flex={70} >M Container</Flex>
+            {/* <Flex flex={70} borderRadius={"md"} p={2} flexDir={"column"} alignItems={"center"} justifyContent={"center"} height={"400px"} >
+              <GiConversation size={100} />
+              <Text fontSize={20} >Select a conversation to start messaging</Text>
+            </Flex> */}
+           <MessageContainer/>
        </Flex>
     </Box>
   )
 }
 
-export default ChatPage
+export default ChatPage;
