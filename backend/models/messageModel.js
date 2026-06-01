f1a5274b7b0f3b63b5e077da21eb6dc6faf9b123
Manmit Pal <manmitpal348@gmail.com>
2026-06-01 15:14:24 +0530

    sendMessage working

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -2,17 +2,11 @@ import mongoose from "mongoose";
 
 
 const messageSchema = new mongoose.Schema({
-    conversationId: {
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Conversation'
-    },
-    sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-    },
+    conversationId: { type: mongoose.Schema.Types.ObjectId, ref: "Conversation" },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     text: String,
 }, {timestamps: true});
 
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,9 @@
 import express from "express"
+import protectRoute from "../middleware/protectRoute.js";
+import {sendMessage} from "../controllers/messageController.js"
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-    res.send("Message Route")
-})
+router.post("/", protectRoute, sendMessage);
 
 export default router;
